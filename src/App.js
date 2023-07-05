3313aa6ecfdeea911305ddd7052577b9fba2d4f9
monoj <monojkumar57859893@gmail.com>
2023-07-05 20:38:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
-import Mint from './components/Mint';
-
+import Mint from "./components/Mint";
+import { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
+  useEffect(() => {
+		AOS.init();
+	}, []);
   return (
-   <div className="App">
-    <rorter>
-      <Mint/>
-    </rorter>
-   </div>
+    <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Mint />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+
   );
 }
 
